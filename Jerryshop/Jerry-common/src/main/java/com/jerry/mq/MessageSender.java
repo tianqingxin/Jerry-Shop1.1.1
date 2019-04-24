4c7ce2c89ffa09cdc4d2fc532a5f4f4e7e11996c
JerryBGM <zhongjie_he@163.com>
2019-04-24 21:25:01 +0800

    版本1.1.1

---
+++ Jerryshop/Jerry-common/src/main/java/com/jerry/mq/MessageSender.java
@@ -0,0 +1,18 @@
+package com.jerry.mq;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+import javax.jms.Destination;
+
+@Component
+public class MessageSender {
+    @Autowired
+    private JmsMessagingTemplate jmsTemplate;
+
+    public void send(String queue_name,String json){
+        Destination destination = new ActiveMQQueue(queue_name);
+        jmsTemplate.convertAndSend(destination,json);
+    }
+}
