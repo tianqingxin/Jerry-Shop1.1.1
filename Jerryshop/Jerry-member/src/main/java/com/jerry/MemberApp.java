36ac65ea999676f642c17943962af12298e6e08f
JerryBGM <zhongjie_he@163.com>
2019-05-01 23:56:26 +0800

    注册中心和创建服务

--- Jerryshop/Jerry-member/src/main/java/com/jerry/MemberApp.java
+++ Jerryshop/Jerry-member/src/main/java/com/jerry/MemberApp.java
@@ -2,8 +2,11 @@ package com.jerry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MemberApp {
     public static void main(String[] args) {
         SpringApplication.run(
---
+++ Jerryshop/Jerry-member/src/main/java/com/jerry/controller/memberController.java
@@ -0,0 +1,17 @@
+package com.jerry.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class memberController {
+    @Value("${server.port}")//获得端口号
+    private String port;
+//    @GetMapping("/hello")
+    @RequestMapping("/hello")
+    public String helloMember(String name){
+        return "hello "+name+",from port:"+port;
+    }
+}
